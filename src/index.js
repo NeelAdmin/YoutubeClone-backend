7a55b8663c79fba146c1e273a4bded0b23ea0299
NeelAdmin <navadiyaneel0736@gmail.com>
2025-08-01 13:04:21 +0530

    -Add Api error and response class. -Add necessary middlewares.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import dotenv from "dotenv";
 import { connectDB } from "./db/index.js";
+import express from "express";
+
+const app = express();
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => console.log(error));
+    app.listen(`${process.env.PORT}`, () => {
+      console.log(`Listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => console.log(error));
