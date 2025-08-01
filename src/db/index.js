a87189fedb8f490fcdadfa4f8ff139dfd87bc432
NeelAdmin <navadiyaneel0736@gmail.com>
2025-08-01 11:21:20 +0530

    -Add database connection to atlas

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+export const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${DB_NAME}`
+    );
+
+    console.log("\n Connected to DB", connectionInstance.connection.host);
+  } catch (error) {
+    console.log("ERROR", error);
+    process.exit(1);
+  }
+};
